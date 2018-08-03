6115d682d3e01ebba90f64caabc2ed38260e6ab9
Matt Farmer <mrfarmer777@gmail.com>
2018-08-03 00:29:25 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,9 +24,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @product}
+    render json: @product
     end
   end
 
